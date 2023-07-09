44c0265867bd0b4ace43969532880e772ca5228e
Oliparay <132256793+Oliparay@users.noreply.github.com>
2023-07-09 14:28:56 +0300

    Update Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -23,7 +23,7 @@ public class Main {
     );
 
     public static void main(String[] args) {
-        System.out.println("Введите выражение (например, V * V или 5 + 5):");
+        System.out.println("Введите выражение чисел от 1 - 10 (например, V * V или 5 + 5):");
         System.out.println(calc(new Scanner(System.in).nextLine()));
     }
 
